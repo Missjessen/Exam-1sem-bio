655d3cebe26fbc62c0728ddb2d6909bb3795ac1c
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-11-22 11:34:33 +0100

    Tilføjet admin showtime og relaterede filer

---
+++ app/models/AdminShowtimeModel.php
@@ -0,0 +1,21 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Exam-1sem-bio/init.php';
+
+class ShowtimeModel {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function addShowtime($movieId, $date, $time) {
+        $sql = "INSERT INTO Showtimes (showtime_id, movie_id, date, time) VALUES (UUID(), :movie_id, :date, :time)";
+        $stmt = $this->db->prepare($sql);
+
+        return $stmt->execute([
+            ':movie_id' => $movieId,
+            ':date' => $date,
+            ':time' => $time
+        ]);
+    }
+}
