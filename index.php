f3e1753d58e5ecf6d4f50e49b53efe74ea4470fd
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-04 01:27:23 +0100

    router ændre af page vol 4

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 
 
 require_once __DIR__ . '/Exam-1sem-bio/init.php';
+require_once BASE_PATH . '/core/autoloader.php';
 
 // Definér den aktuelle side
 $current_page = $_GET['page'] ?? 'home';
