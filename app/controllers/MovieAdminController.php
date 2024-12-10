c5d390a5ff16cbd37948d22e426902b70100721d
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-10 09:49:36 +0100

    index op.

--- app/controllers/MovieAdminController.php
+++ app/controllers/MovieAdminController.php
@@ -102,7 +102,7 @@ class MovieAdminController {
         }
     
         // Redirect tilbage til admin_movie siden
-        header("Location: /Exam-1sem-bio/index.php?page=admin_movie");
+        header("Location: /index.php?page=admin_movie");
         exit;
     }
 
