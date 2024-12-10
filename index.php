a27bf3a418246dc2373be154d44b88e07ba1a4c2
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-10 10:39:59 +0100

    index op.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Inkluder nødvendige filer
-require_once __DIR__ . '/init.php';
-require_once __DIR__ . '/app/models/Security.php';
+require_once __DIR__ . 'init.php';
+require_once __DIR__ . 'app/models/Security.php';
 
 
 // Start session
