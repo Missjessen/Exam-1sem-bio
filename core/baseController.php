6f07570530b893d2be5afada09d1dbd54e218d80
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-10 14:09:37 +0100

    index op.

--- core/baseController.php
+++ core/baseController.php
@@ -1,21 +1,14 @@
-<?php
+<!-- <?php
 require_once 'init.php';
 
 class BaseController {
-    protected $model;
+   
     protected $errorController;
 
 
-    public function __construct($model) {
-        $this->model = $model;
-        $this->errorController = new ErrorController();
-    }
-
-    protected function validateInput($input) {
-        return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-    }
-
     
+
+
     protected function handleError($errorMessage, $statusCode = 500) {
         if ($statusCode === 404) {
             $this->errorController->show404($errorMessage);
@@ -28,3 +21,4 @@ class BaseController {
 
 
 
+ -->
