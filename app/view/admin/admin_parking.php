3c115ddf9b2d0099a3cbf41e91d0db2cc2f231ea
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-12 00:36:01 +0100

    form

--- app/view/admin/admin_parking.php
+++ app/view/admin/admin_parking.php
@@ -2,12 +2,12 @@
 $to = "missejssen87@gmail.com";
 $subject = "Test Email";
 $message = "Dette er en testmail.";
-$headers = "From: missjessen87@cruise-nights-cinema.dk/.com";
+$headers = "From: missjessen87@cruise-nights-cinema.com";
 
 if (mail($to, $subject, $message, $headers)) {
     echo "Mail blev sendt!";
 } else {
-    echo "Mail blev ikke sendt.";
+    echo "Mail blev ikke .";
 }
 
 
