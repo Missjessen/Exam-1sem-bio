35b4efa48987615a0ff441c96f9752b5032d96b4
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-12 10:08:22 +0100

    form

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ $subject = "Test Email via PHP mail()";
 $message = "Dette er en testmail sendt via PHP's mail() funktion.";
 
 // Afsenderens emailadresse (skift til en gyldig emailadresse på dit domæne)
-$from = "noreply@yourdomain.com"; 
+$from = "nanjes01@365essv.dk"; 
 $headers = "From: $from\r\n";
 $headers .= "Reply-To: $from\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
