88c32c59ce6273d82d8a0d7685fc8c653cff20e0
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-13 10:30:27 +0100

    login

--- init.php
+++ init.php
@@ -1,7 +1,6 @@
 <?php
 ob_start(); // Start output buffering for at undgå header-fejl
-echo currentPageURL()
-session_start();
+
 
 // Definér den aktuelle side og slug
 define('BASE_URL', (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . '');
