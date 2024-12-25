69302a3c2a70af228c5fd24c9ef49e0894c2a924
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-25 21:55:55 +0100

    movie_details

--- app/view/Error/404.php
+++ app/view/Error/404.php
@@ -1,5 +1,9 @@
 <body>
     <h1>404 - Page Not Found</h1>
-    <p><?php echo htmlspecialchars($additionalData['message']); ?></p>
+    <?php if (!empty($additionalData['message'])): ?>
+        <p><?= htmlspecialchars($additionalData['message'], ENT_QUOTES, 'UTF-8') ?></p>
+    <?php else: ?>
+        <p>Vi kunne ikke finde den side, du leder efter.</p>
+    <?php endif; ?>
     <a href="?page=homePage">Return to Homepage</a>
 </body>
