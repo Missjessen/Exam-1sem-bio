eb300520c5228fcd0aad944eb1b72e6264133d3a
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-25 17:29:10 +0100

    movie_details

--- app/controllers/MovieFrontendController.php
+++ app/controllers/MovieFrontendController.php
@@ -3,21 +3,22 @@
 class MovieFrontendController {
     private $model;
     //private $recipientEmail = "nsj@cruise-nights-cinema.dk";
-
-    public function __construct(MovieFrontendModel $model) {
-        $this->model = $model;
-    }
-
-    public function showMovieDetails($uuid) {
-        $movie = $this->model->getMovieByUuid($uuid);
+    public function showMovieDetails($slug) {
+        error_log("Slug modtaget: " . $slug);
+    
+        $movie = $this->model->getMovieDetailsBySlug($slug);
         if (!$movie) {
+            error_log("Film ikke fundet for slug: " . $slug);
             header("HTTP/1.0 404 Not Found");
             require_once __DIR__ . '/../view/errors/404.php';
             exit();
         }
-
+    
+        error_log("Film fundet: " . print_r($movie, true));
+    
+        $showtimes = $this->model->getShowingsForMovie($movie['id']);
+        error_log("Visningstider: " . print_r($showtimes, true));
+    
         require_once __DIR__ . '/../view/user/movieDetails.php';
     }
-
-   
-}    
+}
