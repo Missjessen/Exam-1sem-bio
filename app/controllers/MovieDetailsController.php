f1cb2faa9e536d8347c45e1632d3e6ecca788f09
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-25 11:24:08 +0100

    movie_details

--- app/controllers/MovieDetailsController.php
+++ app/controllers/MovieDetailsController.php
@@ -11,31 +11,24 @@ class MovieDetailsController {
 
     public function showMovieDetailsBySlug($slug) {
         try {
-            // Valider, at slug ikke er tom
             if (empty($slug)) {
                 throw new Exception("Slug mangler i URL'en.");
             }
-            if (!preg_match('/^[a-zA-Z0-9-_]+$/', $slug)) {
-                throw new Exception("Slug indeholder ugyldige tegn.");
-            }
-            
-
-            // Hent filmens detaljer
+    
+            error_log("Slug modtaget: " . $slug); // Debug
+    
             $movie = $this->movieModel->getMovieDetailsBySlug($slug);
             if (!$movie) {
-                throw new Exception("Ingen data fundet for slug: $slug");
+                throw new Exception("Filmen blev ikke fundet for slug: $slug");
             }
     
-            // Hent alle visninger for filmen
             $showtimes = $this->movieModel->getShowingsForMovie($movie['id']);
-    
-            // Indlæs siden med filmdata og visningstider
             $this->pageLoader->loadUserPage('movie_details', [
                 'movie' => $movie,
                 'showtimes' => $showtimes
             ]);
         } catch (Exception $e) {
-            $this->handleError("Fejl: " . $e->getMessage());
+            $this->handleError($e->getMessage());
         }
     }
 
