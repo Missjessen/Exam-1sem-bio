3e278d85c9d7e250c55261c6a2b7e581701dad30
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2024-12-25 18:08:10 +0100

    movie_details

--- test3.php
+++ test3.php
@@ -24,10 +24,19 @@ function currentPageURL($page, $additionalParams = []) {
 
     // Opdater query-parametre
     $queryParams = array_merge(['page' => $page], $additionalParams);
-
-    // Generér ny URL
-    return $protocol . "://" . $host . $uri . '?' . http_build_query($queryParams);
-}
+    if (!function_exists('currentPageURL')) {
+        function currentPageURL($page, $additionalParams = []) {
+            $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? "https" : "http");
+            $host = $_SERVER['HTTP_HOST'];
+            $uri = strtok($_SERVER['REQUEST_URI'], '?'); // Fjern eksisterende query-parametre
+    
+            // Opdater query-parametre
+            $queryParams = array_merge(['page' => $page], $additionalParams);
+    
+            // Generér ny URL
+            return $protocol . "://" . $host . $uri . '?' . http_build_query($queryParams);
+        }
+    }
 
 // Test med en dummy slug
 $generatedUrl = currentPageURL('movie_details', ['slug' => 'test-slug']);
